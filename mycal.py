a91be3f2d974e2e7b0354e1c3beeb1b279f3736a
Ajeet Pathak <ajeet.pathak44@gmail.com>
2018-07-15 16:04:32 +0530

    Calcy Program
    Signed-off-by: Ajeet Pathak <ajeet.pathak44@gmail.com>

---
+++ mycal.py
@@ -0,0 +1,10 @@
+class Calcy():
+    def add(a, b):
+        print(a+b)
+    def Substract(a, b):
+        print(a-b)
+    def Multiply(a, b):
+        print(a*b)
+    def Devide(a, b):
+        print(a/b)
+ 
